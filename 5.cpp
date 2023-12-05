f7f0cc62bda42d1f134827a9887b9e390d78675b
Philipp Czerner <suyjuris.gi@nicze.de>
2023-12-05 23:55:05 +0100

    Day 5, attempt 1
    It worked. One bug caught in testing: line 53 used i instead of seeds[i].

--- 5.cpp
+++ 5.cpp
@@ -10,6 +10,7 @@ void parse_int_line(Array_t<u8> text, s64* inout_index, Array_dyn<s64>* out) {
     s64 state = 0;
     s64 i;
     for (i = *inout_index; i < text.size; ++i) {
+        u8 c = text[i];
         if ('0' <= c and c <= '9') {
             number = 10*number + (c - '0');
             state = 1;
@@ -27,7 +28,7 @@ void parse_int_line(Array_t<u8> text, s64* inout_index, Array_dyn<s64>* out) {
 }
 
 int main() {
-    auto arr = array_load_from_file("input/4"_arr);
+    auto arr = array_load_from_file("input/5"_arr);
     
     s64 index = 0;
     Array_dyn<s64> seeds;
@@ -39,11 +40,31 @@ int main() {
         if (arr[index] != ':') continue;
         
         index += 2;
-        while (arr[index] != '\n') {
+        while (index < arr.size and arr[index] != '\n') {
+            mapping.size = 0;
             parse_int_line(arr, &index, &mapping);
             
+            assert(mapping.size == 3);
+            s64 target = mapping[0];
+            s64 source = mapping[1];
+            s64 count  = mapping[2];
+            
+            for (s64 i = 0; i < seeds.size; ++i) {
+                if (source > seeds[i] or seeds[i] >= source+count) continue;
+                array_push_back(&seeds_new, seeds[i] - source + target);
+                seeds[i] = seeds.back();
+                --seeds.size;
+                --i;
+            }
         }
+        
+        array_append(&seeds, seeds_new);
+        seeds_new.size = 0;
     }
     
-    format_print("%d\n", sum);
+    s64 min = seeds[0];
+    for (s64 i: seeds) {
+        if (min > i) min = i;
+    }
+    format_print("%d\n", min);
 }
